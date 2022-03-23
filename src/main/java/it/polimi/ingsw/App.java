239000716782ab2714f28edd7f93690830ef0642
daniPolii <100693976+daniPolii@users.noreply.github.com>
2022-03-23 19:23:40 +0100

    Created maven project structure

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
