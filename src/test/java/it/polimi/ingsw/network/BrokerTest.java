fcaaa923505c82cd3e07462a078c07b268765023
daniPolii <100693976+daniPolii@users.noreply.github.com>
2022-05-09 16:30:43 +0200

    Modified Broker tests

--- src/test/java/it/polimi/ingsw/network/BrokerTest.java
+++ src/test/java/it/polimi/ingsw/network/BrokerTest.java
@@ -9,12 +9,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BrokerTest {
 
     MessageBroker broker;
+
+    public InputStream createJSONFile(String message){
+        File tempFile;
+        InputStream inputStreamFile;
+        OutputStream outputStreamFile;
+        try {
+            tempFile = File.createTempFile("test","idfk");
+            inputStreamFile = new FileInputStream(tempFile);
+            outputStreamFile = new FileOutputStream(tempFile);
+        } catch (IOException e) {
+            fail("Can't create temp file or get streams");
+            return null;
+        }
+        try {
+            outputStreamFile.write(message.getBytes(StandardCharsets.UTF_8));
+            outputStreamFile.flush();
+        } catch (IOException e) {
+            fail("can't write to temp file");
+        }
+        return inputStreamFile;
+    }
 
     @BeforeEach
     public void init(){
@@ -28,29 +48,31 @@ public class BrokerTest {
 
     @Test
     public void deserializeTest(){
-        File tempFile;
-        InputStream inputStreamFile;
-        OutputStream outputStreamFile;
-        try {
-            tempFile = File.createTempFile("test","idfk");
-            inputStreamFile = new FileInputStream(tempFile);
-            outputStreamFile = new FileOutputStream(tempFile);
-        } catch (IOException e) {
-            assertTrue(false, "Can't create temp file or get streams");
-            return;
-        }
-        try {
-            outputStreamFile.write(("{\n" +
-                    "    \"COMMAND\" : \"CONNECTION_REQUEST\",\n" +
-                    "    \"NICKNAME\" : \"gigio\"\n" +
-                    "}").getBytes(StandardCharsets.UTF_8));
-            outputStreamFile.flush();
-        } catch (IOException e) {
-            assertTrue(false, "can't write to temp file");
-            return;
-        }
-        broker.receive(inputStreamFile);
+
+        InputStream stream = createJSONFile("{\n" +
+                "    \"COMMAND\" : \"CONNECTION_REQUEST\",\n" +
+                "    \"NICKNAME\" : \"gigio\"\n" +
+                "}");
+        broker.receive(stream);
         assertEquals(CommandEnum.fromObjectToEnum(broker.readField(NetworkFieldEnum.COMMAND)), CommandEnum.CONNECTION_REQUEST);
         assertEquals((String)broker.readField(NetworkFieldEnum.NICKNAME), "gigio");
     }
+
+    /*
+    @Test
+    public void twoMessagesQuickSuccession(){
+        InputStream stream = createJSONFile("{\n" +
+                "    \"COMMAND\" : \"CONNECTION_REQUEST\",\n" +
+                "    \"NICKNAME\" : \"gigio\"\n" +
+                "}"+"\n" +
+                "{\n" +
+                "    \"COMMAND\" : \"CONNECTION_REQUEST\",\n" +
+                "    \"NICKNAME\" : \"gigio2\"\n" +
+                "}");
+        assertTrue(broker.receive(stream));
+        assertTrue(broker.receive(stream));
+        assertFalse(broker.receive(stream));
+        //If the messages were correctly parsed, the broker should be able to read two different messages correctly
+    }
+    */
 }
